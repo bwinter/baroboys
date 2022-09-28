5412faa3ef8c33b28992e99c8080999cdd2141d7
Brendan Winter <bwinter@vmware.com>
2022-09-27 23:55:47 -0700

    Capture a bunch of updates.

--- setup/steam.sh
+++ setup/steam.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-
-
 # Prep for team by filling in TOS
 echo steam steam/question select "I AGREE" | debconf-set-selections
 echo steam steam/license note "" | debconf-set-selections
@@ -12,24 +10,3 @@ su bwinter_sc81 -c 'mkdir -p "/home/bwinter_sc81/.steam/sdk64"'
 su bwinter_sc81 -c 'mkdir -p "/home/bwinter_sc81/.steam/sdk32"'
 su bwinter_sc81 -c 'ln -s "/home/bwinter_sc81/.local/share/Steam/steamcmd/linux64/steamclient.so" "/home/bwinter_sc81/.steam/sdk64/steamclient.so"'
 su bwinter_sc81 -c 'ln -s "/home/bwinter_sc81/.local/share/Steam/steamcmd/linux32/steamclient.so" "/home/bwinter_sc81/.steam/sdk32/steamclient.so"'
-
-su bwinter_sc81 -c << EOF
-  pushd "/home/bwinter_sc81/.local/share/Daedalic\ Entertainment\ GmbH/"
-  ls
-  /usr/games/steamcmd +force_install_dir './Barotrauma' +login anonymous +app_update 1026340 validate +quit
-EOF
-
-# /usr/games/steamcmd +runscript "./setup/steamcmd_script.txt"'
-
-# su bwinter_sc81 -c "mkdir -p '/home/bwinter_sc81/.local/share/Daedalic Entertainment GmbH/Barotrauma'"
-
-# echo screen -S baro-server -d -m "./barotrauma/DedicatedServer"
-
-# apt-get install -yq screen silversearcher-ag
-
-# apt-get install -yq build-essential \
-#   wget apt-transport-https dirmngr \
-#   apt-transport-https ca-certificates gnupg
-
-
-
