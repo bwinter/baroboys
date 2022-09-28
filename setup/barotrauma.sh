b98c8c4c1dd49829d5f2be77f104fcb889e693b7
Brendan Winter <bwinter@vmware.com>
2022-09-28 00:38:52 -0700

    Whoops ...

--- setup/barotrauma.sh
+++ setup/barotrauma.sh
@@ -4,11 +4,10 @@ su - bwinter_sc81 <<EOF
   /usr/games/steamcmd +force_install_dir '/home/bwinter_sc81/baroboys/Barotrauma' +login anonymous +app_update 1026340 validate +quit
 EOF
 
-# shellcheck disable=SC2016 (I want the HOME of the sub-shell.)
-su bwinter_sc81 -c 'mkdir -p "${HOME}/.local/share/Daedalic Entertainment GmbH/Barotrauma/"'
+su bwinter_sc81 -c 'mkdir -p "home/bwinter_sc81/.local/share/Daedalic Entertainment GmbH/Barotrauma/"'
 
-su bwinter_sc81 -c 'ln -s "${HOME}/baroboys/Barotrauma/Multiplayer" "${HOME}/.local/share/Daedalic Entertainment GmbH/Barotrauma/Multiplayer"'
-su bwinter_sc81 -c 'ln -s "${HOME}/baroboys/Barotrauma/WorkshopMods" "${HOME}/.local/share/Daedalic Entertainment GmbH/Barotrauma/WorkshopMods"'
+su bwinter_sc81 -c 'ln -s "home/bwinter_sc81/baroboys/Barotrauma/Multiplayer" "home/bwinter_sc81/.local/share/Daedalic Entertainment GmbH/Barotrauma/Multiplayer"'
+su bwinter_sc81 -c 'ln -s "home/bwinter_sc81/baroboys/Barotrauma/WorkshopMods" "home/bwinter_sc81/.local/share/Daedalic Entertainment GmbH/Barotrauma/WorkshopMods"'
 
 # echo screen -S baro-server -d -m "./barotrauma/DedicatedServer"
 
