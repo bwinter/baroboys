f1aec65ec200dd1476dd1a0bb788200c8154cb74
Brendan Winter <bwinter@vmware.com>
2022-09-28 09:28:13 -0700

    Make some final updates to the scripts.

--- setup/barotrauma.sh
+++ setup/barotrauma.sh
@@ -2,14 +2,16 @@
 
 su - bwinter_sc81 <<EOF
   /usr/games/steamcmd +force_install_dir '/home/bwinter_sc81/baroboys/Barotrauma' +login anonymous +app_update 1026340 validate +quit
+
+  cd /home/bwinter_sc81/baroboys
+  git checkout -- './Barotrauma/Data/clientpermissions.xml'
+  git checkout -- './Barotrauma/serversettings.xml'
+
+  mkdir -p "/home/bwinter_sc81/.local/share/Daedalic Entertainment GmbH/Barotrauma/"
+
+  ln -sf "/home/bwinter_sc81/baroboys/Barotrauma/Multiplayer" "/home/bwinter_sc81/.local/share/Daedalic Entertainment GmbH/Barotrauma/Multiplayer"
+  ln -sf "/home/bwinter_sc81/baroboys/Barotrauma/WorkshopMods" "/home/bwinter_sc81/.local/share/Daedalic Entertainment GmbH/Barotrauma/WorkshopMods"
 EOF
-
-git checkout -- 'Barotrauma/Data/clientpermissions.xml'
-
-su bwinter_sc81 -c 'mkdir -p "home/bwinter_sc81/.local/share/Daedalic Entertainment GmbH/Barotrauma/"'
-
-su bwinter_sc81 -c 'ln -s "home/bwinter_sc81/baroboys/Barotrauma/Multiplayer" "home/bwinter_sc81/.local/share/Daedalic Entertainment GmbH/Barotrauma/Multiplayer"'
-su bwinter_sc81 -c 'ln -s "home/bwinter_sc81/baroboys/Barotrauma/WorkshopMods" "home/bwinter_sc81/.local/share/Daedalic Entertainment GmbH/Barotrauma/WorkshopMods"'
 
 # echo screen -S baro-server -d -m "./barotrauma/DedicatedServer"
 
--- setup/steam.sh
+++ setup/steam.sh
@@ -8,5 +8,5 @@ apt-get install -yq steamcmd libicu67
 
 su bwinter_sc81 -c 'mkdir -p "/home/bwinter_sc81/.steam/sdk64"'
 su bwinter_sc81 -c 'mkdir -p "/home/bwinter_sc81/.steam/sdk32"'
-su bwinter_sc81 -c 'ln -s "/home/bwinter_sc81/.local/share/Steam/steamcmd/linux64/steamclient.so" "/home/bwinter_sc81/.steam/sdk64/steamclient.so"'
-su bwinter_sc81 -c 'ln -s "/home/bwinter_sc81/.local/share/Steam/steamcmd/linux32/steamclient.so" "/home/bwinter_sc81/.steam/sdk32/steamclient.so"'
+su bwinter_sc81 -c 'ln -sf "/home/bwinter_sc81/.local/share/Steam/steamcmd/linux64/steamclient.so" "/home/bwinter_sc81/.steam/sdk64/steamclient.so"'
+su bwinter_sc81 -c 'ln -sf "/home/bwinter_sc81/.local/share/Steam/steamcmd/linux32/steamclient.so" "/home/bwinter_sc81/.steam/sdk32/steamclient.so"'
