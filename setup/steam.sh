27dabed4171be203525febc1815a22f21995136b
Brendan Winter <bwinter@vmware.com>
2022-09-27 23:18:54 -0700

    Full rehash of the startup scripts.

--- setup/steam.sh
+++ setup/steam.sh
@@ -3,17 +3,17 @@
 # /usr/games/steamcmd +force_install_dir '/home/bwinter_sc81/.local/share/Daedalic Entertainment GmbH/Barotrauma' +login anonymous +app_update ${STEAMAPPID} validate +quit
 
 # Prep for team by filling in TOS
-echo steam steam/question select "I AGREE" | sudo debconf-set-selections
-echo steam steam/license note "" | sudo debconf-set-selections
+echo steam steam/question select "I AGREE" | debconf-set-selections
+echo steam steam/license note "" | debconf-set-selections
 
-sudo apt-get install -yq steamcmd libicu67
+apt-get install -yq steamcmd libicu67
 
-mkdir -p "/home/bwinter_sc81/.steam/sdk64"
-mkdir -p "/home/bwinter_sc81/.steam/sdk32"
-ln -s "/home/bwinter_sc81/.local/share/Steam/steamcmd/linux64/steamclient.so" "/home/bwinter_sc81/.steam/sdk64/steamclient.so"
-ln -s "/home/bwinter_sc81/.local/share/Steam/steamcmd/linux32/steamclient.so" "/home/bwinter_sc81/.steam/sdk32/steamclient.so"
+su bwinter_sc81 -c 'mkdir -p "/home/bwinter_sc81/.steam/sdk64"'
+su bwinter_sc81 -c 'mkdir -p "/home/bwinter_sc81/.steam/sdk32"'
+su bwinter_sc81 -c 'ln -s "/home/bwinter_sc81/.local/share/Steam/steamcmd/linux64/steamclient.so" "/home/bwinter_sc81/.steam/sdk64/steamclient.so"'
+su bwinter_sc81 -c 'ln -s "/home/bwinter_sc81/.local/share/Steam/steamcmd/linux32/steamclient.so" "/home/bwinter_sc81/.steam/sdk32/steamclient.so"'
 
-/usr/games/steamcmd +runscript "/home/bwinter_sc81/.local/share/Daedalic\ Entertainment\ GmbH/setup/steamcmd_script.txt"
+su bwinter_sc81 -c '/usr/games/steamcmd +runscript "/home/bwinter_sc81/.local/share/Daedalic\ Entertainment\ GmbH/setup/steamcmd_script.txt"'
 
 # su bwinter_sc81 -c "mkdir -p '/home/bwinter_sc81/.local/share/Daedalic Entertainment GmbH/Barotrauma'"
 
