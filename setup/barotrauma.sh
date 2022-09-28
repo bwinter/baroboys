1493c5c9a880632f12ac3d578ecb1be47f4b0646
Brendan Winter <bwinter@vmware.com>
2022-09-28 01:09:44 -0700

    Move again.

--- setup/barotrauma.sh
+++ setup/barotrauma.sh
@@ -4,6 +4,8 @@ su - bwinter_sc81 <<EOF
   /usr/games/steamcmd +force_install_dir '/home/bwinter_sc81/baroboys/Barotrauma' +login anonymous +app_update 1026340 validate +quit
 EOF
 
+git checkout -- 'Barotrauma/Data/clientpermissions.xml'
+
 su bwinter_sc81 -c 'mkdir -p "home/bwinter_sc81/.local/share/Daedalic Entertainment GmbH/Barotrauma/"'
 
 su bwinter_sc81 -c 'ln -s "home/bwinter_sc81/baroboys/Barotrauma/Multiplayer" "home/bwinter_sc81/.local/share/Daedalic Entertainment GmbH/Barotrauma/Multiplayer"'
