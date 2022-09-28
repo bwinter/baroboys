6b0049ce8e0ed9497b516209b06911c19bc56ae2
Brendan Winter <bwinter@vmware.com>
2022-09-27 23:16:45 -0700

    Iterating on the git script.

--- setup/git.sh
+++ setup/git.sh
@@ -1,15 +1,13 @@
 #! /bin/bash
 
-sudo su bwinter_sc81
-
 # Need Service Account: git-service-account@europan-world.iam.gserviceaccount.com
 # With Scopes: Secret Manager Secret Accessor
 # Get Github Deploy Key
 # Needs to be saved into secret manager by hand.
-gcloud secrets versions access latest --secret="github-deploy-key" | tee "/home/bwinter_sc81/.ssh/id_ecdsa"
+gcloud secrets versions access latest --secret="github-deploy-key" | su bwinter_sc81 -c 'tee "/home/bwinter_sc81/.ssh/id_ecdsa"'
 chmod 700 "/home/bwinter_sc81/.ssh/id_ecdsa"
 
 # This key is also necessary for github deploy keys.
-echo "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=" | tee "/home/bwinter_sc81/.ssh/known_hosts"
+echo "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=" | su bwinter_sc81 -c 'tee "/home/bwinter_sc81/.ssh/known_hosts"'
 
-git clone git@github.com:bwinter/baroboys.git "/home/bwinter_sc81/.local/share/Daedalic\ Entertainment\ GmbH"
+su bwinter_sc81 -c 'git clone git@github.com:bwinter/baroboys.git "/home/bwinter_sc81/.local/share/Daedalic\ Entertainment\ GmbH"'
