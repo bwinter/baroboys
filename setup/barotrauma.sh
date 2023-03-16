b0a2d458caec9b7a329a6f5f408b8682e4856f2b
Brendan Winter <bwinter@vmware.com>
2023-03-15 21:55:04 -0700

    Add notes for getting LUA working.

--- setup/barotrauma.sh
+++ setup/barotrauma.sh
@@ -3,6 +3,9 @@
 su - bwinter_sc81 <<EOF
   /usr/games/steamcmd +force_install_dir '/home/bwinter_sc81/baroboys/Barotrauma' +login anonymous +app_update 1026340 validate +quit
 
+  # https://steamcommunity.com/sharedfiles/filedetails/?id=2559634234
+  # wget https://github.com/evilfactory/LuaCsForBarotrauma/releases/download/latest/luacsforbarotrauma_build_linux.tar.gz -o /temp/luacsforbarotrauma_build_linux.tar.gz
+
   cd /home/bwinter_sc81/baroboys
   git checkout -- './Barotrauma/Data/clientpermissions.xml'
   git checkout -- './Barotrauma/serversettings.xml'
