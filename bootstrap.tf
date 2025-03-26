714808a7ea7728dc24183e5ae0b514c6ddd80cd1
Brendan Winter <bwinter.sc81@gmail.com>
2025-03-25 22:15:49 -0400

    Include prior manual setup in a tf file.

---
+++ bootstrap.tf
@@ -0,0 +1,37 @@
+# Enable required APIs
+resource "google_project_service" "logging" {
+  service = "logging.googleapis.com"
+}
+
+resource "google_project_service" "monitoring" {
+  service = "monitoring.googleapis.com"
+}
+
+resource "google_project_service" "osconfig" {
+  service = "osconfig.googleapis.com"
+}
+
+# Project-wide metadata to enable guest policies
+resource "google_compute_project_metadata" "osconfig_metadata" {
+  metadata = {
+    enable-guest-attributes = "TRUE"
+    enable-osconfig         = "TRUE"
+  }
+}
+
+# Grant logging and monitoring roles to your custom SA
+resource "google_project_iam_member" "log_writer" {
+  role   = "roles/logging.logWriter"
+  member = "serviceAccount:${var.service_account_email}"
+}
+
+resource "google_project_iam_member" "metric_writer" {
+  role   = "roles/monitoring.metricWriter"
+  member = "serviceAccount:${var.service_account_email}"
+}
+
+# (Optional) Grant guest policy admin permissions to your SA
+resource "google_project_iam_member" "guest_policy_admin" {
+  role   = "roles/osconfig.guestPolicyAdmin"
+  member = "serviceAccount:${var.service_account_email}"
+}
