aa2a1d3c3d845165b58a7ce5427dfac02afeecf9
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-18 10:19:00 -0700

    Initial attempts at running VRising
    - Person has a cool systemd tool for running the server. (I was using screen.)

--- scripts/setup/vrising.sh
+++ scripts/setup/vrising.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 su - bwinter_sc81 <<EOF
-  /usr/games/steamcmd +force_install_dir '/home/bwinter_sc81/baroboys/Vrising' +login anonymous +app_update 1829350 validate +quit
+  /usr/games/steamcmd +force_install_dir '/home/bwinter_sc81/baroboys/VRising' +login anonymous +app_update 1829350 validate +quit
 
   echo "" >> '/home/bwinter_sc81/.profile'
   echo "export EDITOR=vim" >> '/home/bwinter_sc81/.profile'
