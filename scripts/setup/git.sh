d71d4c79512e0f214358f98b14c27b2b585fe724
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-18 14:13:04 -0700

    Consolidate all the git setup into one file.
    - I think this will work. I suspect it's just an oversite during my prior work.

--- scripts/setup/git.sh
+++ scripts/setup/git.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
 
 # Need Service Account: git-service-account@europan-world.iam.gserviceaccount.com
-# With Scopes: Secret Manager Secret Accessor
+# With Scopes: "Secret Manager Secret Accessor"
 # Get Github Deploy Key
 # Needs to be saved into secret manager by hand.
+
+# Root Clone
+mkdir -p "/root/.ssh"
+gcloud secrets versions access latest --secret="github-deploy-key" | tee "/root/.ssh/id_ecdsa"
+chmod 700 "/root/.ssh/id_ecdsa"
+
+# This key is also necessary for github deploy keys.
+echo "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=" | tee "/root/.ssh/known_hosts"
+
+git clone "git@github.com:bwinter/baroboys.git" "/root/baroboys"
+
+# User Clone
 gcloud secrets versions access latest --secret="github-deploy-key" | su bwinter_sc81 -c 'tee "/home/bwinter_sc81/.ssh/id_ecdsa"'
 chmod 700 "/home/bwinter_sc81/.ssh/id_ecdsa"
 
