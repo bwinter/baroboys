b7963f6eb9f79d4f7684b58fac9af24492d2bcc7
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-18 10:12:17 -0700

    Add initial Vrising installer.

---
+++ scripts/setup/vrising.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+su - bwinter_sc81 <<EOF
+  /usr/games/steamcmd +force_install_dir '/home/bwinter_sc81/baroboys/Vrising' +login anonymous +app_update 1829350 validate +quit
+
+  echo "" >> '/home/bwinter_sc81/.profile'
+  echo "export EDITOR=vim" >> '/home/bwinter_sc81/.profile'
+EOF
