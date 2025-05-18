03b7a4a6c1f496e4dc884f49fdcbdc89caa7205d
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-18 13:52:50 -0700

    Add wine apt deps.
    - Also move all apt deps to the above first apt commands. First attempt at simplifying the installer.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -21,23 +21,25 @@ deb http://deb.debian.org/debian bullseye-backports main
 deb-src http://deb.debian.org/debian bullseye-backports main
 " | tee "/etc/apt/sources.list"
 
+### wine
+curl "https://dl.winehq.org/wine-builds/winehq.key" | tee "/usr/share/keyrings/winehq.gpg"
+echo "deb [signed-by=/usr/share/keyrings/winehq.gpg] https://dl.winehq.org/wine-builds/debian/ bookworm main" \
+  | tee "/etc/apt/sources.list.d/winehq.list"
+
+### gcloud
+curl "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | tee "/usr/share/keyrings/cloud.google.gpg"
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" \
+  | tee "/etc/apt/sources.list.d/google-cloud-sdk.list"
+
 # Refresh state.
 dpkg --add-architecture i386
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F24AEA9FB05498B7
 apt-get remove -y --purge man-db
 apt-get -yq update
 apt-get -yq upgrade
-
-# Install initial dependencies
-apt-get install -yq git curl screen silversearcher-ag build-essential wget dirmngr apt-transport-https ca-certificates gnupg
-apt-get autoremove
-
-### gcloud
-curl "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | tee "/usr/share/keyrings/cloud.google.gpg"
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" \
-  | tee "/etc/apt/sources.list.d/google-cloud-sdk.list"
-apt-get -yq update
-apt-get install -yq google-cloud-cli
+apt-get install -yq git curl screen silversearcher-ag build-essential wget dirmngr apt-transport-https ca-certificates gnupg \
+                    google-cloud-cli \
+                    winehq-stable winetricks xvfb
 apt-get autoremove
 
 # echo '"projects/europan-world/zones/us-west1-b/instances/europa","[{""type"":""ops-agent""}]"' > agents_to_install.csv && \
