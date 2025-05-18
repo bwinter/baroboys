a9c07711bc68c0ab0349182cba7ddee171cf0da2
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-18 13:40:18 -0700

    Add a little descriptor.

--- scripts/start_barotrauma_server.sh
+++ scripts/start_barotrauma_server.sh
@@ -1,2 +1,3 @@
 # This is designed to be manually run on the server.
+# tl;dr it starts a screen session so terminal can die and server still stays up.
 screen -S baro-server -d -m /home/bwinter_sc81/baroboys/Barotrauma/DedicatedServer
