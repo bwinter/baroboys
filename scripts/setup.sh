35b49b1cf1fa4c1719cf03e0e44788e6ffa9a510
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-18 14:07:11 -0700

    Hardcoding vrisint switch for now.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -60,5 +60,7 @@ git clone "git@github.com:bwinter/baroboys.git" "/root/baroboys"
 
 source "/root/baroboys/scripts/setup/git.sh"
 source "/root/baroboys/scripts/setup/steam.sh"
+
 # TODO: make this configurable.
+# source "/root/baroboys/scripts/setup/barotrauma.sh"
 source "/root/baroboys/scripts/setup/vrising.sh"
