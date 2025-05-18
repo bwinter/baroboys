3639b4a740d624484da7ee099f6679e6e2710864
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-18 14:13:04 -0700

    Consolidate all the git setup into one file.
    - I think this will work. I suspect it's just an oversite during my prior work.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -45,19 +45,6 @@ apt-get autoremove
 curl -sSO "https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh"
 bash "add-google-cloud-ops-agent-repo.sh" --also-install
 
-# Need Service Account: git-service-account@europan-world.iam.gserviceaccount.com
-# With Scopes: "Secret Manager Secret Accessor"
-# Get Github Deploy Key
-# Needs to be saved into secret manager by hand.
-mkdir -p "/root/.ssh"
-gcloud secrets versions access latest --secret="github-deploy-key" | tee "/root/.ssh/id_ecdsa"
-chmod 700 "/root/.ssh/id_ecdsa"
-
-# This key is also necessary for github deploy keys.
-echo "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=" | tee "/root/.ssh/known_hosts"
-
-git clone "git@github.com:bwinter/baroboys.git" "/root/baroboys"
-
 source "/root/baroboys/scripts/setup/git.sh"
 source "/root/baroboys/scripts/setup/steam.sh"
 
