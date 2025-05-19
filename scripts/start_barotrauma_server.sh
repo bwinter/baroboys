91b2b6a1162d11281e6fbba222345c95c936bebd
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-19 10:08:26 -0700

    Note that Barotrauma should also use systemd

--- scripts/start_barotrauma_server.sh
+++ scripts/start_barotrauma_server.sh
@@ -1,3 +1,5 @@
 # This is designed to be manually run on the server.
 # tl;dr it starts a screen session so terminal can die and server still stays up.
 screen -S baro-server -d -m /home/bwinter_sc81/baroboys/Barotrauma/DedicatedServer
+
+# TODO: get this into a systemd service.
