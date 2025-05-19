ad2b382e4a2c4757149b1af9aef4f6ab4fa028d8
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-19 11:32:37 -0700

    Fixing the setup script.
    - I had forgotten we needed to clone before we had access to some scripts. So now front-loading git install and scripts.
    
    - Little sad but necessary w/o custom image.

--- scripts/setup/install/apt_core.sh
+++ scripts/setup/install/apt_core.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
 set -eux
 
-echo "
-#------------------------------------------------------------------------------#
-#                   OFFICIAL DEBIAN REPOS
-#------------------------------------------------------------------------------#
-
-###### Debian Main Repos
-deb http://deb.debian.org/debian/ bullseye main contrib non-free
-deb-src http://deb.debian.org/debian/ bullseye main contrib non-free
-
-deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free
-deb-src http://deb.debian.org/debian/ bullseye-updates main contrib non-free
-
-deb http://deb.debian.org/debian-security bullseye-security main
-deb-src http://deb.debian.org/debian-security bullseye-security main
-
-deb http://deb.debian.org/debian bullseye-backports main
-deb-src http://deb.debian.org/debian bullseye-backports main
-" | tee "/etc/apt/sources.list"
-
-# Refresh state.
-dpkg --add-architecture i386
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F24AEA9FB05498B7
-apt-get remove -y --purge man-db
-apt-get -yq update
-apt-get -yq upgrade
-apt-get install -yq git curl screen silversearcher-ag build-essential wget dirmngr apt-transport-https ca-certificates gnupg
+apt-get install -yq curl screen silversearcher-ag build-essential wget dirmngr apt-transport-https ca-certificates gnupg
--- scripts/setup/setup.sh
+++ scripts/setup/setup.sh
@@ -1,6 +1,58 @@
 #!/bin/bash
 set -eux
 
+# Git is a dependency that needs immediate install.
+echo "
+#------------------------------------------------------------------------------#
+#                   OFFICIAL DEBIAN REPOS
+#------------------------------------------------------------------------------#
+
+###### Debian Main Repos
+deb http://deb.debian.org/debian/ bullseye main contrib non-free
+deb-src http://deb.debian.org/debian/ bullseye main contrib non-free
+
+deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free
+deb-src http://deb.debian.org/debian/ bullseye-updates main contrib non-free
+
+deb http://deb.debian.org/debian-security bullseye-security main
+deb-src http://deb.debian.org/debian-security bullseye-security main
+
+deb http://deb.debian.org/debian bullseye-backports main
+deb-src http://deb.debian.org/debian bullseye-backports main
+" | tee "/etc/apt/sources.list"
+
+# Refresh state.
+dpkg --add-architecture i386
+apt-get -yq update
+apt-get install -yq debian-archive-keyring
+apt-get remove -y --purge man-db
+apt-get -yq update
+apt-get -yq upgrade
+apt-get install -yq git
+
+# Pull deploy key from GCP secret manager
+gcloud secrets versions access latest --secret="github-deploy-key" | tee "/tmp/id_ecdsa"
+install -m 600 -o bwinter_sc81 -g bwinter_sc81 /tmp/id_ecdsa
+
+# Need Service Account: git-service-account@europan-world.iam.gserviceaccount.com
+# With Scopes: "Secret Manager Secret Accessor"
+# Get Github Deploy Key
+# Needs to be saved into secret manager by hand.
+
+# Root SSH setup
+mkdir -p "/root/.ssh"
+chmod 700 "/root/.ssh"
+
+cat "/tmp/id_ecdsa" > "/root/.ssh/id_ecdsa"
+chmod 600 '/root/.ssh/id_ecdsa'
+
+# Add GitHub host key
+echo "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=" | tee "/root/.ssh/known_hosts"
+
+[ -d "/root/baroboys/.git" ] || git clone "git@github.com:bwinter/baroboys.git" "/root/baroboys"
+
+sudo -u bwinter_sc81 -- "/home/bwinter_sc81/baroboys/scripts/setup/root/clone_repo.sh"
+
 # Core install
 source "/root/baroboys/scripts/setup/install/apt_core.sh"
 source "/root/baroboys/scripts/setup/install/apt_gcloud.sh"
@@ -8,10 +60,8 @@ source "/root/baroboys/scripts/setup/install/apt_wine.sh"
 source "/root/baroboys/scripts/setup/install/apt_xvfb.sh"
 source "/root/baroboys/scripts/setup/install/apt_steam.sh"
 
-# Clone repo for both users
-source "/root/baroboys/scripts/setup/root/clone_repo.sh"
-
 # Run game-specific setup
+echo "🎮 Setting up game mode: ${ACTIVE_GAME:-undefined}"
 source "/root/baroboys/scripts/setup/root/setup_game.sh"
 
 apt-get autoremove
