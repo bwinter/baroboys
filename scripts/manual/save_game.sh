deac5322e5c69b02a813446ebb5f9e5b343df59e
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-19 10:48:10 -0700

    Build out game "mode" tooling.

--- scripts/manual/save_game.sh
+++ scripts/manual/save_game.sh
@@ -4,4 +4,4 @@ set -eux
 gcloud compute ssh bwinter_sc81@europa \
   --project=europan-world \
   --zone=us-west1-b \
-  --command="/home/bwinter_sc81/baroboys/scripts/teardown/user/save_vrising.sh"
+  --command="/home/bwinter_sc81/baroboys/scripts/teardown/user/save_game.sh"
--- scripts/teardown/root/shutdown.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# sudo -u bwinter_sc81 -- "/home/bwinter_sc81/baroboys/scripts/teardown/user/save_barotrauma.sh"
-sudo -u bwinter_sc81 -- "/home/bwinter_sc81/baroboys/scripts/teardown/user/save_vrising.sh"
