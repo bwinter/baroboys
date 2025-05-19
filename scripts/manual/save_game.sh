0177f45c552fe085e27c372939a814b1de60c2a2
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-19 11:05:53 -0700

    Clean up.
    - Removing a redundant make target / script.
    - Updating README step-by-step with new make targets.

--- scripts/manual/save_game.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eux
-
-gcloud compute ssh bwinter_sc81@europa \
-  --project=europan-world \
-  --zone=us-west1-b \
-  --command="/home/bwinter_sc81/baroboys/scripts/teardown/user/save_game.sh"
