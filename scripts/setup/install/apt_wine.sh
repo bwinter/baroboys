af2ca4fa7c60b230504ea493bbd571ce727fd7b3
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-21 02:53:23 -0700

    Okay, I think this should make winetricks a bit less verbose too.

--- scripts/setup/install/apt_wine.sh
+++ scripts/setup/install/apt_wine.sh
@@ -12,7 +12,8 @@ echo "deb [signed-by=/usr/share/keyrings/winehq.gpg] https://dl.winehq.org/wine-
 apt-get -yq update
 apt-get install -yq winehq-stable winetricks xvfb
 
-sudo -u bwinter_sc81 -- xvfb-run --auto-servernum --server-args='-screen 0 1024x768x24' env \
-                          WINETRICKS_GUI=none \
-                          WINEDEBUG=-all \
-                          winetricks allfonts || echo echo "⚠️ Winetricks fonts failed, continuing anyway"
+sudo -u bwinter_sc81 --  env \
+                          WINETRICKS_GUI=none WINEDEBUG=-all \
+                          xvfb-run --auto-servernum --server-args='-screen 0 1024x768x24' \
+                          winetricks --unattended corefonts tahoma \
+                          || echo echo "⚠️ Winetricks fonts failed, continuing anyway"
