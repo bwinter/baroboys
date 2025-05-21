d7cbf9b3466842f2f1ded235e94ba77da140ebed
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-21 13:35:28 -0700

    Pull xvfb from the correct location.

--- scripts/setup/root/setup_vrising.sh
+++ scripts/setup/root/setup_vrising.sh
@@ -2,7 +2,7 @@
 set -eux
 
 # Start xvfb
-install -m 644 "/root/baroboys/scripts/services/xvfb.service" "/etc/systemd/system/"
+install -m 644 "/root/baroboys/scripts/systemd/xvfb.service" "/etc/systemd/system/"
 systemctl daemon-reexec
 systemctl daemon-reload
 systemctl enable xvfb.service
@@ -13,7 +13,7 @@ systemctl status xvfb.service
 sudo -u bwinter_sc81 -- "/home/bwinter_sc81/baroboys/scripts/setup/user/install_vrising.sh"
 
 # Register and start the systemd service
-install -m 644 "/root/baroboys/scripts/services/vrising.service" "/etc/systemd/system/"
+install -m 644 "/root/baroboys/scripts/systemd/vrising.service" "/etc/systemd/system/"
 systemctl daemon-reexec
 systemctl daemon-reload
 systemctl enable vrising.service
