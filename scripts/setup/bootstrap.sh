d48c395c8d3adc1c6765c45b1824f6321af8cb0d
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-21 03:14:04 -0700

    Speedup feedback - put gcp observability later.

--- scripts/setup/bootstrap.sh
+++ scripts/setup/bootstrap.sh
@@ -3,9 +3,9 @@ set -eux
 
 # Core install
 source "/root/baroboys/scripts/setup/install/apt_core.sh"
-source "/root/baroboys/scripts/setup/install/apt_gcloud.sh"
 source "/root/baroboys/scripts/setup/install/apt_wine.sh"
 source "/root/baroboys/scripts/setup/install/apt_steam.sh"
+source "/root/baroboys/scripts/setup/install/apt_gcloud.sh"
 
 # Refresh repo, just in case.
 source "/root/baroboys/scripts/setup/clone_repo.sh"
