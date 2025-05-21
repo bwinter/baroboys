482483aecaa406128b137bf8c30a6101006b51cd
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-21 16:04:37 -0700

    Attempt to install vrising configs as part of setup.

--- scripts/setup/user/install_vrising.sh
+++ scripts/setup/user/install_vrising.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -eux
 
-/usr/games/steamcmd +@sSteamCmdForcePlatformType windows \
+/usr/games/steamcmd \
+  +@sSteamCmdForcePlatformType windows \
   +force_install_dir "$HOME/baroboys/VRising" \
   +login anonymous \
   +app_update 1829350 validate \
   +quit
 
-echo 1829350 > "$HOME/baroboys/VRising/steam_appid.txt"
-
-
-# TODO: Restore save game
-# git checkout ...
+# Restore canonical server configs
+cd "$HOME/baroboys"
+git checkout -- \
+  VRising/VRisingServer_Data/StreamingAssets/Settings/ServerGameSettings.json \
+  VRising/VRisingServer_Data/StreamingAssets/Settings/ServerHostSettings.json
 
 # Ensure EDITOR is set for future shell sessions
 grep -qxF 'export EDITOR=vim' "$HOME/.profile" || echo 'export EDITOR=vim' >> "$HOME/.profile"
