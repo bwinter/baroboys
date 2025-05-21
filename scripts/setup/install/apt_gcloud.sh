bff90a9d0a3969fff2f4f7b462de74d700ab5c9d
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-21 01:04:49 -0700

    Debugging apt_gcloud.sh

--- scripts/setup/install/apt_gcloud.sh
+++ scripts/setup/install/apt_gcloud.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -eux
 
-curl "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | tee "/usr/share/keyrings/cloud.google.gpg"
+# Install the Google Cloud SDK APT key
+curl -fsSL "https://packages.cloud.google.com/apt/doc/apt-key.gpg" \
+  | gpg --dearmor -o "/usr/share/keyrings/cloud.google.gpg"
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" \
   | tee "/etc/apt/sources.list.d/google-cloud-sdk.list"
 
@@ -9,3 +11,7 @@ apt-get install -yq google-cloud-cli
 
 curl -sSO "https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh"
 bash "add-google-cloud-ops-agent-repo.sh" --also-install
+
+
+# Add the signed repo using the keyring file
+
