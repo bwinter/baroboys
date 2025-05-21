af3b085bff017e6b5989032b0eceacda26ea7bcf
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-21 03:14:04 -0700

    Speedup feedback - put gcp observability later.

--- scripts/setup/install/apt_gcloud.sh
+++ scripts/setup/install/apt_gcloud.sh
@@ -15,5 +15,3 @@ curl -sSO "https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh"
 bash "add-google-cloud-ops-agent-repo.sh" --also-install
 
 
-# Add the signed repo using the keyring file
-
