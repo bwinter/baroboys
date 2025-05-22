2558606bafce7095a869506a42b191169bddc027
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-21 18:23:02 -0700

    Also checkout admin list.

--- scripts/setup/user/install_vrising.sh
+++ scripts/setup/user/install_vrising.sh
@@ -12,7 +12,9 @@ set -eux
 cd "$HOME/baroboys"
 git checkout -- \
   VRising/VRisingServer_Data/StreamingAssets/Settings/ServerGameSettings.json \
-  VRising/VRisingServer_Data/StreamingAssets/Settings/ServerHostSettings.json
+  VRising/VRisingServer_Data/StreamingAssets/Settings/ServerHostSettings.json \
+  VRising/Data/Settings/adminlist.txt \
+  VRising/Data/Settings/banlist.txt
 
 # Ensure EDITOR is set for future shell sessions
 grep -qxF 'export EDITOR=vim' "$HOME/.profile" || echo 'export EDITOR=vim' >> "$HOME/.profile"
