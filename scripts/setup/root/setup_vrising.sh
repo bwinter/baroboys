212286197ffb89d7983f853c5710dc17879a47f0
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-26 13:40:17 -0700

    Disable autostart of VRising.
    - Need to have some boot stuff occur first.
    - Add helpful message.

--- scripts/setup/root/setup_vrising.sh
+++ scripts/setup/root/setup_vrising.sh
@@ -42,6 +42,10 @@ sudo -u bwinter_sc81 -- "/home/bwinter_sc81/baroboys/scripts/setup/user/install_
 install -m 644 "/root/baroboys/scripts/systemd/vrising.service" "/etc/systemd/system/"
 systemctl daemon-reexec
 systemctl daemon-reload
-systemctl enable vrising.service
-systemctl start vrising.service
+# Disabling, I want the boot.service to make this happen by running this script.
+# systemctl enable vrising.service
+if [ "${ACTIVE_GAME:-}" = "vrising" ]; then
+  echo "🎮 Starting V Rising server via systemd..."
+  systemctl start vrising.service
+fi
 systemctl status vrising.service
