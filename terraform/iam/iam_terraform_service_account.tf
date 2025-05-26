08aa625a69fdf99d5a47eb5f19977e0c880e272c
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-26 02:30:40 -0700

    Add service account creation automation.

---
+++ terraform/iam/iam_terraform_service_account.tf
@@ -0,0 +1,51 @@
+locals {
+  terraform_sa_member = "serviceAccount:terraform@${var.project}.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "terraform_compute_admin" {
+  project = var.project
+  role    = "roles/compute.admin"
+  member  = local.terraform_sa_member
+}
+
+resource "google_project_iam_member" "terraform_compute_security_admin" {
+  project = var.project
+  role    = "roles/compute.securityAdmin"
+  member  = local.terraform_sa_member
+}
+
+resource "google_project_iam_member" "terraform_iam_service_account_user" {
+  project = var.project
+  role    = "roles/iam.serviceAccountUser"
+  member  = local.terraform_sa_member
+}
+
+resource "google_project_iam_member" "terraform_logging_log_writer" {
+  project = var.project
+  role    = "roles/logging.logWriter"
+  member  = local.terraform_sa_member
+}
+
+resource "google_project_iam_member" "terraform_monitoring_metric_writer" {
+  project = var.project
+  role    = "roles/monitoring.metricWriter"
+  member  = local.terraform_sa_member
+}
+
+resource "google_project_iam_member" "terraform_osconfig_guest_policy_admin" {
+  project = var.project
+  role    = "roles/osconfig.guestPolicyAdmin"
+  member  = local.terraform_sa_member
+}
+
+resource "google_project_iam_member" "terraform_iam_role_admin" {
+  project = var.project
+  role    = "roles/iam.roleAdmin"
+  member  = local.terraform_sa_member
+}
+
+resource "google_project_iam_member" "terraform_project_iam_admin" {
+  project = var.project
+  role    = "roles/resourcemanager.projectIamAdmin"
+  member  = local.terraform_sa_member
+}
