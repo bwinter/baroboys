278a9f413b9b7fe78b7b37fcb926d21e06ffa73a
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-28 13:57:03 -0700

    Cleanup webserver. Try to make status dynamic.

--- scripts/setup/install/flask_server/webhook_server.py
+++ scripts/setup/install/flask_server/webhook_server.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
 import subprocess
 from datetime import datetime
 
-app = Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="/opt/baroboys/templates")
+
 
 @app.route("/trigger-shutdown", methods=["POST"])
 def trigger_shutdown():
     subprocess.Popen(["systemctl", "start", "shutdown.service"])
     now = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
     return f"<h2>🟠 Shutdown triggered at {now}</h2>"
+
 
 @app.route("/check-status", methods=["GET"])
 def check_status():
--- scripts/setup/install/flask_webhook.sh
+++ scripts/setup/install/flask_webhook.sh
@@ -5,19 +5,19 @@ apt update
 apt install -y python3-pip
 pip3 install flask
 
-
 cp "/root/baroboys/scripts/setup/install/scripts/systemd/baroboys-webhook.service" "/etc/systemd/system/"
 chmod 644 "/etc/systemd/system/baroboys-webhook.service"
 
-cp "/root/baroboys/scripts/setup/install/scripts/setup/install/flash_server/webhook_server.py" "/opt/baroboys/webhook_server.py"
+cp "/root/baroboys/scripts/setup/install/flask_server/webhook_server.py" "/opt/baroboys/webhook_server.py"
 chmod 755 "/opt/baroboys/webhook_server.py"
 
 mkdir -p "/opt/baroboys/static"
-cp "/root/baroboys/scripts/setup/install/assets/admin.html" "/opt/baroboys/static/admin.html"
+cp "/root/baroboys/scripts/setup/install/flask_server/admin.html" "/opt/baroboys/static/admin.html"
 chmod 644 "/opt/baroboys/static/admin.html"
 
-cp "/root/baroboys/scripts/setup/install/assets/templates/status_check.html" "/opt/baroboys/static/templates/status_check.html"
-chmod 644 "/opt/baroboys/static/admin.html"
+mkdir -p "/opt/baroboys/templates"
+cp "/root/baroboys/scripts/setup/install/flask_server/templates/status.html" "/opt/baroboys/templates/status.html"
+chmod 644 "/opt/baroboys/templates/status.html"
 
 systemctl daemon-reload
 systemctl enable baroboys-webhook.service
