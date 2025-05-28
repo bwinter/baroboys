547d603708e121910564117eec10d261b8170ad5
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-28 14:28:00 -0700

    Rework nginx and vrising log folder creation.

--- scripts/setup/install/apt_nginx.sh
+++ scripts/setup/install/apt_nginx.sh
@@ -8,12 +8,5 @@ gcloud secrets versions access latest --secret=nginx-htpasswd \
   > "/etc/nginx/.htpasswd"
 chmod 640 "/etc/nginx/.htpasswd"
 
-chmod o+rx "/home/bwinter_sc81"
-chmod o+rx "/home/bwinter_sc81/baroboys"
-chmod o+rx "/home/bwinter_sc81/baroboys/VRising"
-chmod o+rx "/home/bwinter_sc81/baroboys/VRising/logs"
-
 cp "/root/baroboys/scripts/setup/install/assets/nginx.config" "/etc/nginx/sites-available/vrising-logs"
 ln -s "/etc/nginx/sites-available/vrising-logs" "/etc/nginx/sites-enabled/vrising-logs"
-
-systemctl reload nginx
--- scripts/setup/user/install_vrising.sh
+++ scripts/setup/user/install_vrising.sh
@@ -16,6 +16,12 @@ git checkout -- \
   VRising/Data/Settings/adminlist.txt \
   VRising/Data/Settings/banlist.txt
 
+mkdir -p "/home/bwinter_sc81/baroboys/VRising/logs"
+chmod o+rx "/home/bwinter_sc81"
+chmod o+rx "/home/bwinter_sc81/baroboys"
+chmod o+rx "/home/bwinter_sc81/baroboys/VRising"
+chmod o+rx "/home/bwinter_sc81/baroboys/VRising/logs"
+
 touch "/home/bwinter_sc81/baroboys/VRising/logs/VRisingServer.log"
 chown bwinter_sc81:bwinter_sc81  "/home/bwinter_sc81/baroboys/VRising/logs/VRisingServer.log"
 chmod 644  "/home/bwinter_sc81/baroboys/VRising/logs/VRisingServer.log"
