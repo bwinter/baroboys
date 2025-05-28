31d1b21a648986305fdb7a9e694f697fa57ac85b
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-28 14:06:28 -0700

    Rework systemd service setup.

--- scripts/setup/install/services.sh
+++ scripts/setup/install/services.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -eux
 
-cp "/root/baroboys/scripts/systemd/startup.service" "/etc/systemd/system/startup.service"
-cp "/root/baroboys/scripts/systemd/shutdown.service" "/etc/systemd/system/shutdown.service"
-chmod 644 "/etc/systemd/system/startup.service"
-chmod 644 "/etc/systemd/system/shutdown.service"
-systemctl daemon-reexec
-systemctl daemon-reload
-systemctl enable startup.service
+source "/root/baroboys/scripts/setup/install/setup.sh"
+source "/root/baroboys/scripts/setup/install/shutdown.sh"
+source "/root/baroboys/scripts/setup/install/flask_webhook.sh"
