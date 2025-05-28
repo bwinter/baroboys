b47240d37b3609cfa992227e1925d1091635e1bf
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-28 13:29:05 -0700

    Initial version of shutdown server.

--- scripts/setup/install/apt_nginx.sh
+++ scripts/setup/install/apt_nginx.sh
@@ -6,7 +6,6 @@ apt install -y nginx apache2-utils
 
 gcloud secrets versions access latest --secret=nginx-htpasswd \
   > "/etc/nginx/.htpasswd"
-chown root:root "/etc/nginx/.htpasswd"
 chmod 640 "/etc/nginx/.htpasswd"
 
 chmod o+rx "/home/bwinter_sc81"
@@ -18,9 +17,9 @@ touch "/home/bwinter_sc81/baroboys/VRising/logs/VRisingServer.log"
 touch "/home/bwinter_sc81/baroboys/VRising/logs/startup.log"
 touch "/home/bwinter_sc81/baroboys/VRising/logs/shutdown.log"
 
-chmod o+r "/home/bwinter_sc81/baroboys/VRising/logs/*.log"
+chmod o+r /home/bwinter_sc81/baroboys/VRising/logs/*.log
 
-cp "./assets/nginx.config" "/etc/nginx/sites-available/vrising-logs"
+cp "/root/baroboys/scripts/setup/install/assets/nginx.config" "/etc/nginx/sites-available/vrising-logs"
 ln -s "/etc/nginx/sites-available/vrising-logs" "/etc/nginx/sites-enabled/vrising-logs"
 
 systemctl reload nginx
