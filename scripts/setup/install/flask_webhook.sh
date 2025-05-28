f37763e23f18d9735e616b9b1ece77920445332d
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-28 13:29:05 -0700

    Initial version of shutdown server.

--- scripts/setup/install/flask_webhook.sh
+++ scripts/setup/install/flask_webhook.sh
@@ -5,10 +5,14 @@ apt update
 apt install -y python3-pip
 pip3 install flask
 
-cp "./scripts/setup/webhook_server.py" "/opt/baroboys/webhook_server.py"
-cp "./scripts/systemd/baroboys-webhook.service" "/etc/systemd/system/"
+cp "/root/baroboys/scripts/setup/install/scripts/setup/webhook_server.py" "/opt/baroboys/webhook_server.py"
+cp "/root/baroboys/scripts/setup/install/scripts/systemd/baroboys-webhook.service" "/etc/systemd/system/"
 chmod 755 "/opt/baroboys/webhook_server.py"
-chmod 644 "/etc/systemd/system/"
+chmod 644 "/etc/systemd/system/baroboys-webhook.service"
+
+mkdir -p "/opt/baroboys/static"
+cp "/root/baroboys/scripts/setup/install/assets/admin.html" "/opt/baroboys/static/admin.html"
+chmod 644 "/opt/baroboys/static/admin.html"
 
 systemctl daemon-reload
 systemctl enable baroboys-webhook.service
