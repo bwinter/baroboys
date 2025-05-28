3e8885a7461ad4336c1abc0dc766be78d19d82ee
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-28 14:44:41 -0700

    Ugh, little typo in boostrapper scripts.

--- scripts/setup/install/services.sh
+++ scripts/setup/install/services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eux
 
-source "/root/baroboys/scripts/setup/install/service/setup.sh"
+source "/root/baroboys/scripts/setup/install/service/startup.sh"
 source "/root/baroboys/scripts/setup/install/service/shutdown.sh"
 source "/root/baroboys/scripts/setup/install/service/flask_webhook.sh"
