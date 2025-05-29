a028d904ed1c331f3bff962ffdcb809b8314c49b
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-28 23:20:58 -0700

    Attempt to make the copying of the server a little cleaner.

--- scripts/setup/install/service/flask_webhooks.sh
+++ scripts/setup/install/service/flask_webhooks.sh
@@ -17,20 +17,19 @@ cp "/root/baroboys/scripts/setup/install/flask_server/webhook_server.py" \
 chmod 755 "/opt/baroboys/webhook_server.py"
 
 # Static HTML and assets
-mkdir -p "/opt/baroboys/static"
-cp "/root/baroboys/scripts/setup/install/flask_server/admin.html" \
-   "/opt/baroboys/static/admin.html"
-cp "/root/baroboys/scripts/setup/install/flask_server/favicon.ico" \
-   "/opt/baroboys/static/favicon.ico"
-echo -e "User-agent: *\nDisallow: /" > "/opt/baroboys/static/robots.txt"
+mkdir -p /opt/baroboys/static
+for file in admin.html favicon.ico robots.txt; do
+  cp "/root/baroboys/scripts/setup/install/flask_server/${file}" \
+     "/opt/baroboys/static/${file}"
+done
 chmod 644 /opt/baroboys/static/*
 
 # Jinja templates
-mkdir -p "/opt/baroboys/templates"
-cp "/root/baroboys/scripts/setup/install/flask_server/templates/status.html" \
-   "/opt/baroboys/templates/status.html"
-cp "/root/baroboys/scripts/setup/install/flask_server/templates/404.html" \
-   "/opt/baroboys/templates/404.html"
+mkdir -p /opt/baroboys/templates
+for file in 404.html status.html directory.html; do
+  cp "/root/baroboys/scripts/setup/install/flask_server/templates/${file}" \
+     "/opt/baroboys/templates/${file}"
+done
 chmod 644 /opt/baroboys/templates/*.html
 
 # Activate Flask service
