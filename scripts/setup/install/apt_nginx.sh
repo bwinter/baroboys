9481247a47ef2ceac368ee5ada63551b2eed143f
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-28 19:23:35 -0700

    Rename the nginx site to be more accurate.

--- scripts/setup/install/apt_nginx.sh
+++ scripts/setup/install/apt_nginx.sh
@@ -8,5 +8,5 @@ gcloud secrets versions access latest --secret=nginx-htpasswd \
   > "/etc/nginx/.htpasswd"
 chmod 640 "/etc/nginx/.htpasswd"
 
-cp "/root/baroboys/scripts/setup/install/assets/nginx.config" "/etc/nginx/sites-available/vrising-logs"
-ln -s "/etc/nginx/sites-available/vrising-logs" "/etc/nginx/sites-enabled/vrising-logs"
+cp "/root/baroboys/scripts/setup/install/assets/nginx.config" "/etc/nginx/sites-available/vrising-admin"
+ln -s "/etc/nginx/sites-available/vrising-admin" "/etc/nginx/sites-enabled/vrising-admin"
