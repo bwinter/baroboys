bc51fbe81587e4b152e2353cba88fa2e1331f5bf
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-28 22:13:58 -0700

    Update the web server.
    - Having issues with usability.

--- scripts/setup/install/apt_nginx.sh
+++ scripts/setup/install/apt_nginx.sh
@@ -6,7 +6,8 @@ apt install -y nginx apache2-utils
 
 gcloud secrets versions access latest --secret=nginx-htpasswd \
   > "/etc/nginx/.htpasswd"
-chmod 640 "/etc/nginx/.htpasswd"
+chmod 644 "/etc/nginx/.htpasswd"
+
 
 cp "/root/baroboys/scripts/setup/install/assets/nginx.config" "/etc/nginx/sites-available/vrising-admin"
 ln -s "/etc/nginx/sites-available/vrising-admin" "/etc/nginx/sites-enabled/vrising-admin"
