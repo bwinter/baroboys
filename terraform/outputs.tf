6d7bad1db08b07764b8e39e390ac633d1e4bd6fb
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-28 23:57:50 -0700

    Make terraform generate the admin link.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,8 @@ output "google_caller_identity" {
 }
 
 data "google_client_openid_userinfo" "me" {}
+
+output "admin_server_url" {
+  description = "Direct link to the Admin page."
+  value       = "http://${google_compute_instance.default.network_interface[0].access_config[0].nat_ip}:8080/"
+}
