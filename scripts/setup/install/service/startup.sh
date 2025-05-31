f97c8d7d2c626d5996f6d3108c2de147f42d19ba
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-31 02:52:15 -0700

    Add reset breaks to all the logs.

--- scripts/setup/install/service/startup.sh
+++ scripts/setup/install/service/startup.sh
@@ -5,7 +5,8 @@ mkdir -p "/home/bwinter_sc81/baroboys/VRising/logs/"
 chown bwinter_sc81:bwinter_sc81  "/home/bwinter_sc81/baroboys/VRising/logs/"
 chmod 700  "/home/bwinter_sc81/baroboys/VRising/logs/"
 
-touch "/home/bwinter_sc81/baroboys/VRising/logs/startup.log"
+touch  "/home/bwinter_sc81/baroboys/VRising/logs/startup.log"
+printf "\n==== %s ====\n" "$(date +%Y%m%d-%H%M)" >> "/home/bwinter_sc81/baroboys/VRising/logs/startup.log"
 chown bwinter_sc81:bwinter_sc81  "/home/bwinter_sc81/baroboys/VRising/logs/startup.log"
 chmod 644  "/home/bwinter_sc81/baroboys/VRising/logs/startup.log"
 
