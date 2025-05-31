12bcb9ea5ffe0cf2449396968a3239012496275c
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-30 22:23:30 -0700

    Attempting to strip out hard coded password.

--- scripts/setup/user/install_vrising.sh
+++ scripts/setup/user/install_vrising.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -eux
 
+# Fetch the RCON password from GCP Secret Manager
+RCON_PASSWORD="$(gcloud secrets versions access latest --secret=server-password)"
+
+# Paths
+HOST_JSON="VRising/VRisingServer_Data/StreamingAssets/Settings/ServerHostSettings.json"
+GAME_JSON="VRising/VRisingServer_Data/StreamingAssets/Settings/ServerGameSettings.json"
+
 /usr/games/steamcmd \
   +@sSteamCmdForcePlatformType windows \
   +force_install_dir "$HOME/baroboys/VRising" \
@@ -11,10 +18,13 @@ set -eux
 # Restore canonical server configs
 cd "$HOME/baroboys"
 git checkout -- \
-  VRising/VRisingServer_Data/StreamingAssets/Settings/ServerGameSettings.json \
-  VRising/VRisingServer_Data/StreamingAssets/Settings/ServerHostSettings.json \
-  VRising/Data/Settings/adminlist.txt \
-  VRising/Data/Settings/banlist.txt
+  "$HOST_JSON" \
+  "$GAME_JSON" \
+  "VRising/Data/Settings/adminlist.txt" \
+  "VRising/Data/Settings/banlist.txt"
+
+jq --arg pass "$RCON_PASSWORD" '.Password = $pass' -i "$HOST_JSON"
+jq --arg pass "$RCON_PASSWORD" '.Rcon.Password = $pass' -i "$HOST_JSON"
 
 mkdir -p "/home/bwinter_sc81/baroboys/VRising/logs"
 chmod o+rx "/home/bwinter_sc81"
