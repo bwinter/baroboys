6f8dc78528ee2a07e625a7f128f1917db39e7940
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-30 20:05:46 -0700

    Okay, try to fix the user updater.

--- scripts/setup/root/setup_users.sh
+++ scripts/setup/root/setup_users.sh
@@ -3,4 +3,12 @@ set -eux
 
 # Refresh root
 source "/root/baroboys/scripts/setup/util/refresh_repo.sh"
-sudo -u bwinter_sc81 -- "/usr/bin/sudo /root/baroboys/scripts/setup/util/refresh_repo.sh"
+
+cp "/root/baroboys/scripts/setup/util/refresh_repo.sh" "/tmp/refresh_repo.sh"
+chown bwinter_sc81:bwinter_sc81 "/tmp/refresh_repo.sh"
+chmod 644  "/tmp/refresh_repo.sh"
+
+# Now run it as the target user
+sudo -u bwinter_sc81 -- "/tmp/refresh_repo.sh"
+
+rm -f "/tmp/refresh_repo.sh"
