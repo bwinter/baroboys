e717d61c1ec79da66b859906c6aa40a35a21ff92
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-30 19:54:44 -0700

    Try and sudo inside the sudo!

--- scripts/setup/root/setup_users.sh
+++ scripts/setup/root/setup_users.sh
@@ -3,4 +3,4 @@ set -eux
 
 # Refresh root
 source "/root/baroboys/scripts/setup/util/refresh_repo.sh"
-sudo -u bwinter_sc81 -- "/home/bwinter_sc81/baroboys/scripts/setup/util/refresh_repo.sh"
+sudo -u bwinter_sc81 -- "/usr/bin/sudo /root/baroboys/scripts/setup/util/refresh_repo.sh"
