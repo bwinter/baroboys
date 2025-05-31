297bb04e183685700e71596aba42ec7216b4bbdf
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-30 20:45:47 -0700

    Lets reorder the startup script a little. Make it so the script trys to get admin going asap.

--- scripts/setup/startup.sh
+++ scripts/setup/startup.sh
@@ -4,17 +4,17 @@ set -eux
 # Refreshes Repositories.
 source "/root/baroboys/scripts/setup/root/setup_users.sh"
 
+# Refreshes Nginx & config (Get latest Admin Server routes.)
+source "/root/baroboys/scripts/setup/install/apt_nginx.sh"
+
+# Refreshes & Enables & Starts Admin Server (Startup Admin Server immediately.)
+source "/root/baroboys/scripts/setup/install/service/admin_server.sh"
+
 # Refreshes Shutdown Service (Want to use latest lave logic.)
 source "/root/baroboys/scripts/setup/install/service/shutdown.sh"
 
 # Refreshes & Enables Startup Service (Want to install self to ensure refresh occurs after restart.)
 source "/root/baroboys/scripts/setup/install/service/startup.sh"
-
-# Refreshes & Enables & Starts Admin Server (Startup Admin Server immediately.)
-source "/root/baroboys/scripts/setup/install/service/admin_server.sh"
-
-# Refreshes Nginx & config (Get latest Admin Server routes.)
-source "/root/baroboys/scripts/setup/install/apt_nginx.sh"
 
 # Refreshes game (Dependencies refreshed, game updated & started.)
 source "/root/baroboys/scripts/setup/root/setup_game.sh"
