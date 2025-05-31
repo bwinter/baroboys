2e8ac5a5a063c2cdb9be18c06c3e120ecbe349bf
Brendan Winter <bwinter.sc81@gmail.com>
2025-05-30 20:08:25 -0700

    Give repos execute perms.

--- scripts/setup/root/setup_users.sh
+++ scripts/setup/root/setup_users.sh
@@ -6,7 +6,7 @@ source "/root/baroboys/scripts/setup/util/refresh_repo.sh"
 
 cp "/root/baroboys/scripts/setup/util/refresh_repo.sh" "/tmp/refresh_repo.sh"
 chown bwinter_sc81:bwinter_sc81 "/tmp/refresh_repo.sh"
-chmod 644  "/tmp/refresh_repo.sh"
+chmod 755  "/tmp/refresh_repo.sh"
 
 # Now run it as the target user
 sudo -u bwinter_sc81 -- "/tmp/refresh_repo.sh"
