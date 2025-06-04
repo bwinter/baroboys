845563ef6373f420c3c20b7d76a5034e8576f78b
Brendan Winter <bwinter.sc81@gmail.com>
2025-06-04 00:04:37 -0700

    Have terraform use the key.json as nature intended

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@
 // ─────────────────────────────────────────────────────────────────────────────
 
 provider "google" {
-  //credentials = file("${path.module}/../.secrets/europan-world-terraform-key.json")
+  credentials = file("${path.module}/../.secrets/europan-world-terraform-key.json")
   project = var.project
   region  = var.region
 }
