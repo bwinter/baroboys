8b652101cb0b7ce2bdd5491c51e0fad56f84eaf3
Brendan Winter <bwinter.sc81@gmail.com>
2025-06-05 12:10:32 -0700

    Unzip as part of game install.
    - Kind of the symetric finish. (I noticed the zip removed the save - so it wasn't acting like a backup. And in a newly paved VM it might be good to force the save out of the zip as well. (I presume they can handle fallback but lets just get parity because it's more zen and removes an unknown.)

--- scripts/setup/user/install_vrising.sh
+++ scripts/setup/user/install_vrising.sh
@@ -6,12 +6,28 @@ SERVER_PASSWORD="$(gcloud secrets versions access latest --secret=server-passwor
 export SERVER_PASSWORD
 
 # Paths
-HOST_JSON="VRising/VRisingServer_Data/StreamingAssets/Settings/ServerHostSettings.json"
-GAME_JSON="VRising/VRisingServer_Data/StreamingAssets/Settings/ServerGameSettings.json"
+VRISING_DIR="$HOME/baroboys/VRising"
+SAVE_DIR="$VRISING_DIR/Data/Saves/v4/TestWorld-1"
+HOST_JSON="$VRISING_DIR/VRisingServer_Data/StreamingAssets/Settings/ServerHostSettings.json"
+GAME_JSON="$VRISING_DIR/VRisingServer_Data/StreamingAssets/Settings/ServerGameSettings.json"
 
+# Ensure save is uncompressed
+cd "$SAVE_DIR"
+if ! compgen -G "AutoSave_*.save" > /dev/null; then
+  echo "🗜 No .save found, attempting to decompress most recent .save.gz..."
+  latest_gz=$(find . -name 'AutoSave_*.save.gz' | sed -E 's/.*AutoSave_([0-9]+)\.save\.gz/\1 \0/' | sort -n | tail -n1 | cut -d' ' -f2)
+  if [[ -n "$latest_gz" ]]; then
+    gunzip -kf "$latest_gz"
+    echo "✅ Decompressed: $latest_gz"
+  else
+    echo "⚠️ No compressed autosaves found to restore!"
+  fi
+fi
+
+# Update game files via SteamCMD
 /usr/games/steamcmd \
   +@sSteamCmdForcePlatformType windows \
-  +force_install_dir "$HOME/baroboys/VRising" \
+  +force_install_dir "$VRISING_DIR" \
   +login anonymous \
   +app_update 1829350 validate \
   +quit
@@ -21,8 +37,8 @@ cd "$HOME/baroboys"
 git checkout -- \
   "$HOST_JSON" \
   "$GAME_JSON" \
-  "VRising/Data/Settings/adminlist.txt" \
-  "VRising/Data/Settings/banlist.txt"
+  "$VRISING_DIR/Data/Settings/adminlist.txt" \
+  "$VRISING_DIR/Data/Settings/banlist.txt"
 
 envsubst < "$HOST_JSON" > "$HOST_JSON.tmp"
 mv "$HOST_JSON.tmp" "$HOST_JSON"
@@ -35,10 +51,5 @@ printf "\n==== %s ====\n" "$(date +%Y%m%d-%H%M)" >> "$VRISING_DIR/logs/VRisingSe
 chown bwinter_sc81:bwinter_sc81 "$VRISING_DIR/logs/VRisingServer.log"
 chmod 644 "$VRISING_DIR/logs/VRisingServer.log"
 
-touch "/home/bwinter_sc81/baroboys/VRising/logs/VRisingServer.log"
-printf "\n==== %s ====\n" "$(date +%Y%m%d-%H%M)" >> "/home/bwinter_sc81/baroboys/VRising/logs/VRisingServer.log"
-chown bwinter_sc81:bwinter_sc81  "/home/bwinter_sc81/baroboys/VRising/logs/VRisingServer.log"
-chmod 644  "/home/bwinter_sc81/baroboys/VRising/logs/VRisingServer.log"
-
-# Ensure EDITOR is set for future shell sessions
+# Ensure EDITOR is set for shell sessions
 grep -qxF 'export EDITOR=vim' "$HOME/.profile" || echo 'export EDITOR=vim' >> "$HOME/.profile"
