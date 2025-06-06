9ce3d4a96e755cfe6fef3983b1cca1e4b01614d8
Brendan Winter <bwinter.sc81@gmail.com>
2025-06-06 14:00:46 -0700

    Fix debian warning.

--- scripts/setup/install/apt_core.sh
+++ scripts/setup/install/apt_core.sh
@@ -8,17 +8,17 @@ echo "
 #------------------------------------------------------------------------------#
 
 ###### Debian Main Repos
-deb http://deb.debian.org/debian/ bookworm main contrib non-free
-deb-src http://deb.debian.org/debian/ bookworm main contrib non-free
+deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
 
-deb http://deb.debian.org/debian/ bookworm-updates main contrib non-free
-deb-src http://deb.debian.org/debian/ bookworm-updates main contrib non-free
+deb http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
 
-deb http://deb.debian.org/debian-security bookworm-security main
-deb-src http://deb.debian.org/debian-security bookworm-security main
+deb http://deb.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
 
-deb http://deb.debian.org/debian bookworm-backports main
-deb-src http://deb.debian.org/debian bookworm-backports main
+deb http://deb.debian.org/debian bookworm-backports main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian bookworm-backports main contrib non-free non-free-firmware
 " | tee "/etc/apt/sources.list"
 
 # Refresh state and install git for cloning.
