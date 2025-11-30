31afdaec49c10cac6f70d74c5a007036bfc7927c
Brendan Winter <bwinter.sc81@gmail.com>
2025-11-29 16:13:32 -0800

    Move a comment to a location that is more appropriate.
    - Seems like I moved some functionality but forgot to move a comment with it.

--- scripts/setup/install/service/startup.sh
+++ scripts/setup/install/service/startup.sh
@@ -10,6 +10,7 @@ printf "\n==== %s ====\n" "$(date +%Y%m%d-%H%M)" >> "/var/log/baroboys/vm-startu
 chown bwinter_sc81:bwinter_sc81  "/var/log/baroboys/vm-startup.log"
 chmod 644  "/var/log/baroboys/vm-startup.log"
 
+# Refreshes & Enables & Starts Admin Server (Startup Admin Server immediately.)
 cp "/root/baroboys/scripts/systemd/vm-startup.service" "/etc/systemd/system/vm-startup.service"
 chmod 644 "/etc/systemd/system/vm-startup.service"
 
