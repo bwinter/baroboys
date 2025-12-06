dc8985620692a48395aa054c184b7cd10c635d67
Brendan Winter <bwinter.sc81@gmail.com>
2025-12-05 19:45:30 -0800

    Remove some whitespace.

--- scripts/services/vrising/save.sh
+++ scripts/services/vrising/save.sh
@@ -51,8 +51,6 @@ done
 git add "$gzipped_file"
 git commit -m "Auto-save before shutdown $(date -u +'%Y-%m-%d %H:%M:%S UTC')" || echo "Nothing to commit"
 
-
-
 # Stash local state, pull, and push
 git stash push --include-untracked --quiet || echo "Nothing to stash"
 git pull --rebase
