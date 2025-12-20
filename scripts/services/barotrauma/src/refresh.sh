68bac2feae1cef185a7a4835de95bec317cc3091
Brendan Winter <bwinter.sc81@gmail.com>
2025-12-20 10:18:17 -0600

    Attempt to checkout player perm presets.
    - Wondering if a checkout like this is somehow causing issues.

--- scripts/services/barotrauma/src/refresh.sh
+++ scripts/services/barotrauma/src/refresh.sh
@@ -8,6 +8,7 @@ export SERVER_PASSWORD
 # Paths
 BAROTRAUMA_DIR="${HOME}/baroboys/Barotrauma"
 CLIENT_PERMISSIONS_XML="$BAROTRAUMA_DIR/Data/clientpermissions.xml"
+PERMISSION_PRESETS_XML="$BAROTRAUMA_DIR/Data/permissionpresets_player.xml"
 SERVER_SETTINGS_XML="$BAROTRAUMA_DIR/serversettings.xml"
 
 echo "=== BEFORE steamcmd ==="
@@ -35,6 +36,7 @@ find ~/.steam -maxdepth 3 -type f 2>/dev/null || true
 cd "$HOME/baroboys"
 git checkout -- \
   "$CLIENT_PERMISSIONS_XML" \
+  "$PERMISSION_PRESETS_XML" \
   "$SERVER_SETTINGS_XML"
 
 envsubst < "$SERVER_SETTINGS_XML" > "$SERVER_SETTINGS_XML.tmp"
